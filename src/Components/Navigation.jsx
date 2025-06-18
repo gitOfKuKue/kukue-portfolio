18f894abbf32e5577549225717b9c3dc6537ec46
gitOfKuKue <kukue014@gmail.com>
2025-06-19 02:12:28 +0700

    fixing menu btn

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -108,28 +108,23 @@ const Navigation = ({ current }) => {
           />
         </Link>
       </div>
-      <FontAwesomeIcon
-        icon={menuIcon}
-        onClick={openMenuBtn}
-        id="menuToggle"
-        className="text-2xl w-8 duration-1000 xs:block lg:hidden"
-      />
-      {/* <svg
+
+      <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
         strokeWidth={1.5}
         stroke="currentColor"
-        className="size-6 text-font xs:block lg:hidden"
+        className={`size-7 text-font xs:block lg:hidden duration-300 transition-transform ${menuOpen && "text-accent rotate-90 scale-110"}`}
         onClick={openMenuBtn}
         id="menuToggle"
       >
         <path
           strokeLinecap="round"
           strokeLinejoin="round"
-          d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+          d = {!menuOpen ? "M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" : "M6 18 18 6M6 6l12 12"}
         />
-      </svg> */}
+      </svg>
     </nav>
   );
 };
