24a897f330bde68be9c1e3685bc922884d0d733a
gitOfKuKue <kukue014@gmail.com>
2025-06-23 01:31:49 +0700

    changing tag pic

--- src/Components/Testing.jsx
+++ src/Components/Testing.jsx
@@ -1,36 +1,9 @@
-import React, { useState } from "react";
+import React from 'react'
 
-function ContactForm() {
-  fetch("http://localhost:3001/endpoint", {
-    method: "POST", // or PUT
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name: "John",
-      message: "Hello from React!",
-    }),
-  })
-    .then((res) => res.json())
-    .then((data) => console.log("Success:", data))
-    .catch((error) => console.error("Error:", error));
-
+const Testing = () => {
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Your name"
-      />
-      <textarea
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        placeholder="Your message"
-      />
-      <button type="submit">Send</button>
-    </form>
-  );
+    <div>Testing</div>
+  )
 }
 
-export default ContactForm;
+export default Testing
